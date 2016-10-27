11d525243c52ab7f142e66b1d38d3ab0e04bbc74
David Wood <davidmartynwood@gmail.com>
2016-10-27 22:37:35 +0100

    Fix so that giving the Sphero a new heading works (giving it a new "forward").
    Previously you could rotate Sphero but it would forget the rotation as soon as you moved it. The solution looks a bit hacky but it works. Need to go through the RobotKit library and make it properly async because it's rather confusing at the moment.

--- Sphero.Wpf/ViewModels/SpheroViewModel.cs
+++ Sphero.Wpf/ViewModels/SpheroViewModel.cs
@@ -11,119 +11,126 @@ using Windows.UI.Xaml.Media;
 
 namespace SpheroController.Wpf.ViewModels
 {
-	public class SpheroViewModel : BindableBase
-	{
-		private readonly ISphero sphero;
+    public class SpheroViewModel : BindableBase
+    {
+        private readonly ISphero sphero;
 
-		private Color color = Colors.Black;
+        private Color color = Colors.Black;
 
-		private bool tailLight = false;
-		private double rollAngle;
-		private double rollDistance;
-		private double rotateAngle;
+        private bool tailLight = false;
+        private double rollAngle;
+        private double rollDistance;
+        private double rotateAngle;
 
-		public SpheroViewModel(ISphero sphero)
-		{
-			this.sphero = sphero;
-		}
+        public SpheroViewModel(ISphero sphero)
+        {
+            this.sphero = sphero;
+        }
 
-		public string Name
-		{
-			get
-			{
-				return $"{this.sphero.Name} ({this.sphero.BluetoothName})";
-			}
-		}
+        public string Name
+        {
+            get
+            {
+                return $"{this.sphero.Name} ({this.sphero.BluetoothName})";
+            }
+        }
 
-		public Color Color
-		{
-			get
-			{
-				return color;
-			}
+        public Color Color
+        {
+            get
+            {
+                return color;
+            }
 
-			set
-			{
-				if (this.SetProperty(ref color, value))
-				{
-					this.sphero.SetRGBLED(value.R, value.G, value.B);
-				}
-			}
-		}
+            set
+            {
+                if (this.SetProperty(ref color, value))
+                {
+                    this.sphero.SetRGBLED(value.R, value.G, value.B);
+                }
+            }
+        }
 
-		public bool TailLight
-		{
-			get
-			{
-				return this.tailLight;
-			}
+        public bool TailLight
+        {
+            get
+            {
+                return this.tailLight;
+            }
 
-			set
-			{
-				if (this.SetProperty(ref this.tailLight, value))
-				{
-					this.sphero.SetBackLED(value ? 1.0f : 0.0f);
-				}
-			}
-		}
+            set
+            {
+                if (this.SetProperty(ref this.tailLight, value))
+                {
+                    this.sphero.SetBackLED(value ? 1.0f : 0.0f);
+                }
+            }
+        }
 
-		public double RollAngle
-		{
-			get
-			{
-				return this.rollAngle;
-			}
+        public double RollAngle
+        {
+            get
+            {
+                return this.rollAngle;
+            }
 
-			set
-			{
-				if (this.SetProperty(ref this.rollAngle, value))
-				{
-					this.Roll();
-				}
-			}
-		}
+            set
+            {
+                if (this.SetProperty(ref this.rollAngle, value))
+                {
+                    this.Roll();
+                }
+            }
+        }
 
-		public double RollDistance
-		{
-			get
-			{
-				return this.rollDistance;
-			}
+        public double RollDistance
+        {
+            get
+            {
+                return this.rollDistance;
+            }
 
-			set
-			{
-				if (this.SetProperty(ref this.rollDistance, value))
-				{
-					this.Roll();
-				}
-			}
-		}
+            set
+            {
+                if (this.SetProperty(ref this.rollDistance, value))
+                {
+                    this.Roll();
+                }
+            }
+        }
 
-		public double RotateAngle
-		{
-			get
-			{
-				return this.rotateAngle;
-			}
+        public double RotateAngle
+        {
+            get
+            {
+                return this.rotateAngle;
+            }
 
-			set
-			{
-				if (this.SetProperty(ref this.rotateAngle, value))
-				{
-					this.sphero.Roll((int)this.rotateAngle, 0);
-				}
-			}
-		}
+            set
+            {
+                if (this.SetProperty(ref this.rotateAngle, value))
+                {
+                    
+                    this.sphero.Roll((int)this.rotateAngle, 0);
 
-		private void Roll()
-		{
-			if (this.RollDistance > 0)
-			{
-				this.sphero.Roll((int)this.RollAngle, (float)this.RollDistance / 100);
-			} else
-			{
-				this.sphero.Roll(0, 0f);
-			}
-		}
-	}
+                    // For Sphero to have a new heading we need to rotate and then call SetHeading
+                    // apparently with a delay in the middle
+                    Task.Delay(1000).Wait(2000);
+
+                    this.sphero.SetHeading(0);
+                }
+            }
+        }
+
+        private void Roll()
+        {
+            if (this.RollDistance > 0)
+            {
+                this.sphero.Roll((int)this.RollAngle, (float)this.RollDistance / 100);
+            } else
+            {
+                this.sphero.Roll(0, 0f);
+            }
+        }
+    }
 }
